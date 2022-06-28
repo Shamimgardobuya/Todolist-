083f85b9d213dc9a798b51baa6830a133e3a23a3
Shamimgardobuya <shamimobuya@gmail.com>
2022-06-28 22:27:49 +0300

    makinga to do list

---
+++ todoing.py
@@ -0,0 +1,75 @@
+#create a class of todos and using def init create unique attributes
+#create empty list
+#append the object
+#create function to loop through and print all the objects,
+
+#create empty list and append the items
+#create condition to access the items
+#if completed ,remove using .pop
+#
+# class Todo:
+#     task2=str(input("Enter a task"))
+#     def __init__(self,task):
+#         self.task=task
+#     lista=[]
+#     lista.append(Todo("Finishing Kotlin recycler view"))
+#     lista.append(Todo("Reading originals"))
+#     for task in lista:
+# 
+#         print(task)
+
+# task=str(input("Enter a task")) 
+# quantity=int(input("How many tasks would you wish to add"))
+
+# def Tasking():
+#   def trying():  
+#    task=str(input("Enter a task"))
+  
+#    quantity=int(input("How many tasks would you wish to add"))
+#    p=[]
+#    p.append(task)
+#    print(p) 
+#    if len(p)<quantity:
+#     quest=str("Do you still wish to add another task?Yes or No ").lower()
+#     if quest=="yes":
+#         trying()
+
+#   trying()
+#   # p=[]
+  
+        
+#         # for i in p:
+#         #     print(p)
+# #    addying()
+
+# Tasking()
+#   #request user for input 
+   #request forusers input
+   #create a list and append the input
+   #keep on appending the task to list
+# task=str(input("Enter a task"))
+quantity=int(input("How many tasks would you wish to add"))
+p=[]
+while len(p)<quantity:
+     
+     def tasking():
+        task2=str(input("Enter a task"))
+        p.append(task2)
+
+     tasking()
+print(p)
+def done():
+  removing=str(input("Do you wish to remove an item "))
+  for i in p:
+# tasking()
+# print(p)
+
+
+  
+
+
+
+
+
+
+  
