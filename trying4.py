baa83ad54be58292d3428126cb06d6366cf832d1
Shamimgardobuya <shamimobuya@gmail.com>
2022-07-03 22:15:10 +0300

    using recursion for addying item

--- trying4.py
+++ trying4.py
@@ -15,6 +15,6 @@ def summing(number):  #create function and pass in paramter
 
         # start.append(number)
         # print(number)
-a=summing(7)                             #create a variable  that'll hold the function.
+b=summing(7)                             #create a variable  that'll hold the function.
 
-print(a)
+print(b)
